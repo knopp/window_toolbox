5545a0a14702d10b4eba14516dc84b4bfaaeac7e
Jon Ihlas <j.ihlas@betclicgroup.com>
2026-05-06 16:10:54 +0200

    Fix move window to left or top monitor

--- lib/src/win32_util.dart
+++ lib/src/win32_util.dart
@@ -7,8 +7,10 @@ import 'package:ffi/ffi.dart';
 import 'package:win32/win32.dart';
 
 (int, int) splitLParam(int lParam) {
-  final x = lParam & 0xFFFF;
-  final y = (lParam >> 16) & 0xFFFF;
+  int x = lParam & 0xFFFF;
+  int y = (lParam >> 16) & 0xFFFF;
+  if (x >= 0x8000) x -= 0x10000;
+  if (y >= 0x8000) y -= 0x10000;
   return (x, y);
 }
 
